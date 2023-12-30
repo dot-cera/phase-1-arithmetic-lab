13ac2f19e4f65e91bb70e3b8b46569ba54901a94
dot-cera <ritaclaris980@gmail.com>
2023-12-30 21:47:58 +0000

    assignement

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1=2;
+const num2=31;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random()*10) 
+console.log(random)
 
-// Write your code here
+const num3 = 22
+const num4 = 9
+const mod = num3 % num4;
+
+let max;
+max = Math.max(2, 3, 4, 6, 16, 18, 20)
